6a4bccfdd9a8dc25fa64f0559d79158cfba28dbc
Qwintyn <q.rice17@hotmail.com>
2023-07-17 21:35:12 -0400

    done

--- src/ca/concordia/algos/FibonacciVariantAnalyzer.java
+++ src/ca/concordia/algos/FibonacciVariantAnalyzer.java
@@ -1,31 +1,32 @@
 package ca.concordia.algos;
 public class FibonacciVariantAnalyzer {
-
-    private static int calls = 0; //counter to keep track of recursive calls
+    // a counter to keep track of the number of recursive calls
+    private static int calls = 0;
    
     public static int Fibcode(int n) {
         //base case which will return the starting value of the sequence if any of these conditions are met
         if (n == 0 || n == 1 || n == 2 || n == 3) {
             //incrementing the call
             calls++;
+            //if the case is true, return 1
             return 1;
         }
-        //incrementing the call
+        //increment number of the calls
         calls++;
-        //recursive call that will return the values at each given order in the sequence
+        //recursive case that will return the values at each given order in the sequence
         int temp = (3 * Fibcode(n - 3)) + Fibcode(n - 1);
         return temp;
 
     }
 
     public static void main(String[] args) {
-        //setting the input value
+        //setting the input value to 8
         int n = 8;
         //looping through the input value to print each element in the sequence
         for (int i = 0; i < n; i++) {
             System.out.print(Fibcode(i) + " ");
         }
-        //printing the number of calls
+        //print the number of calls
         System.out.println("\n" + "Calls: " + calls);
 
     }
